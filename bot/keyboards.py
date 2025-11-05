32aa65431b0c51ac656ffa62d68a55ec382254b5
Илья <neonfoxik1@gmail.ccom>
2025-11-05 22:40:55 +0300

    12

--- bot/keyboards.py
+++ bot/keyboards.py
@@ -6,7 +6,7 @@ from telebot.types import (
 
 main_markup = InlineKeyboardMarkup()
 btn1 = InlineKeyboardButton("🌐 Наш сайт 🌐", url="https://example.com")
-btn2 = InlineKeyboardButton("⭐Наши отзывы ⭐", url="https://t.me/GearXdevGroup")
-btn3 = InlineKeyboardButton("📝 Заказать айт или бота", url="https://t.me/GearXdev")
+btn2 = InlineKeyboardButton("⭐ Наши отзывы ⭐", url="https://t.me/GearXdevGroup")
+btn3 = InlineKeyboardButton("📝 Заказать cайт или бота", url="https://t.me/GearXdev")
 main_markup.add(btn1).add(btn2).add(btn3)
 
